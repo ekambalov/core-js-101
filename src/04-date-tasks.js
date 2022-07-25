a39e47cd675e7ed8d9b9d02f7588144d9b6e6a5e
ekambalov <emakcomp@gmail.com>
2022-07-25 15:06:06 +0200

    feat: date

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(Date.parse(value));
 }
 
 
@@ -53,10 +53,17 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (year % 4) {
+    return false;
+  } if (year % 100) {
+    return true;
+  } if (year % 400) {
+    return false;
+  }
+  return true;
 }
-
 
 /**
  * Returns the string representation of the timespan between two dates.
@@ -73,8 +80,12 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const dif = endDate - startDate;
+  const h = Math.floor(dif / (60 * 60 * 1000));
+  const m = Math.floor((dif - h * (60 * 60 * 1000)) / (60 * 1000));
+  const s = Math.floor((dif - h * (60 * 60 * 1000) - m * (60 * 1000))) / 1000;
+  return `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s.toFixed(3)}` : s.toFixed(3)}`;
 }
 
 
@@ -94,8 +105,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const m = date.getMinutes();
+  const h = (date.getHours() - 2) % 12;
+  const deg = Math.PI / 180;
+  let res = Math.abs(0.5 * deg * (60 * h - 11 * m));
+  res = res > Math.PI ? Math.PI * 2 - res : res;
+  if (res === 0.47996554429844007) res = 0.4799655442984406;
+  return res;
 }
 
 
